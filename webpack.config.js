01eb24cad03a46a027ace83da70be08df46af3bb
jean-roger orsini <jeanroger.orsini@gmail.com>
2018-06-12 09:23:36 +0900

    App up & running

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
 		]
 	},
 	devServer: {
-		contentBase: path.join(__dirname, 'public'),
+		contentBase: path.join(__dirname, 'dist'),
 		historyApiFallback: true
 	},
 	devtool: 'cheap-module-eval-source-map'
