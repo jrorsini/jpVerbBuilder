9620985eec62130abef98904b32add7b491fe202
jean-roger orsini <jeanroger.orsini@gmail.com>
2018-06-14 11:34:07 +0900

    Created actions and verb reducers.

--- src/lab/redux.js
+++ src/lab/redux.js
@@ -1,3 +1,40 @@
 import { createStore } from 'redux';
 
-const store = createStore(() => {});
+const verbSample = {
+	id: '12345',
+	kanji: '食べる',
+	hiragana: 'たべる',
+	meaning: 'to eat'
+};
+const initialState = [];
+
+/**
+ ACTIONS */
+const addVerb = verb => ({
+	type: 'ADD_VERB',
+	verb
+});
+
+/**
+ REDUCERS */
+
+const verbReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case 'ADD_VERB':
+			return [...state.concat(action.verb)];
+			break;
+
+		default:
+			break;
+	}
+};
+
+const store = createStore(verbReducer);
+
+store.subscribe(() => {
+	console.log(store.getState());
+});
+
+store.dispatch(addVerb(verbSample));
+store.dispatch(addVerb(verbSample));
+store.dispatch(addVerb(verbSample));
