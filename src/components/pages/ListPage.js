115a57549964684220c820c57e16fe89ebe5a165
jean-roger orsini <jeanroger.orsini@gmail.com>
2018-06-15 14:27:47 +0900

    Listing verbs.

--- src/components/pages/ListPage.js
+++ src/components/pages/ListPage.js
@@ -1,5 +1,17 @@
-export const List = () => (
-	<div>
-		<p>List</p>
-	</div>
-);
+import React from 'react';
+import { connect } from 'react-redux';
+
+const List = props => {
+	console.log(props);
+	return (
+		<div>
+			{props.verbs && <ul>{props.verbs.map(verb => <li>{verb.kanji}</li>)}</ul>}
+		</div>
+	);
+};
+
+const mapStateToProps = state => {
+	return state;
+};
+
+export default connect(mapStateToProps)(List);
