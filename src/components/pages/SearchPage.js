1f6cdc37c6c8629dc8fbbb3f0fac549602d8f7cb
jean-roger orsini <jeanroger.orsini@gmail.com>
2018-06-15 11:31:55 +0900

    Displaying search Engine now.

--- src/components/pages/SearchPage.js
+++ src/components/pages/SearchPage.js
@@ -5,26 +5,8 @@ import SearchBar from '../SearchBar';
 /**
 	Search engine looking for verbs.s
  */
-class SearchPage extends React.Component {
-	state = {
-		errorMessage: null,
-		verbPreview: {
-			kanji: null,
-			hiragana: null,
-			meaning: null,
-			exampleList: null
-		},
-		verbs: []
-	};
-
-	render() {
-		return (
-			<div>
-				<SearchBar />
-				{this.state.verbPreview.kanji && (
-					<VerbItem {...this.state.verbPreview} />
-				)}
-			</div>
-		);
-	}
-}
+export default () => (
+	<div>
+		<SearchBar />
+	</div>
+);
