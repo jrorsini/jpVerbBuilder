402a22b7aa43075206996ebe712e53df5e420654
Jean-Roger <jeanroger.orsini@gmail.com>
2018-06-17 23:26:21 +0900

    Added component.

--- src/components/WordList.js
+++ src/components/WordList.js
@@ -1,1 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+const WordList = props => {
+	console.log(props);
+	return (
+		<ul>
+			<li>test</li>
+			<li>test</li>
+			<li>test</li>
+			<li>test</li>
+		</ul>
+	);
+};
+
+const mapStateToProps = state => {
+	return state;
+};
+
+export default connect()(WordList);
--- src/components/pages/ListPage.js
+++ src/components/pages/ListPage.js
@@ -1,17 +1,10 @@
 import React from 'react';
+import WordList from '../WordList';
 import { connect } from 'react-redux';
 
 const List = props => {
 	console.log(props);
-	return (
-		<div className="container">
-			{props.verbs && (
-				<ul>
-					{props.verbs.map(verb => <li key={verb.kanji}>{verb.kanji}</li>)}
-				</ul>
-			)}
-		</div>
-	);
+	return <div className="container">{props.verbs && <WordList />}</div>;
 };
 
 const mapStateToProps = state => {
@@ -19,3 +12,9 @@ const mapStateToProps = state => {
 };
 
 export default connect(mapStateToProps)(List);
+
+{
+	/* <ul>
+					{props.verbs.map(verb => <li key={verb.kanji}>{verb.kanji}</li>)}
+				</ul> */
+}
