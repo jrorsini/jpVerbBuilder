bd5b7c7fe5797b914117ed1544a08b0a361560ee
jean-roger orsini <jeanroger.orsini@gmail.com>
2018-06-18 14:50:13 +0900

    Verb reducers.

--- src/reducers/verbs.js
+++ src/reducers/verbs.js
@@ -2,10 +2,18 @@ const initialState =
 	localStorage.getItem('verbs') !== null
 		? JSON.parse(localStorage.getItem('verbs'))
 		: [];
+
 export default (state = initialState, action) => {
 	switch (action.type) {
 		case 'ADD_VERB':
-			return state.concat(action.verb);
+			let isAlreadyThere = 0;
+			state.map(verb => {
+				if (verb.kanji === action.verb.kanji) {
+					isAlreadyThere = 1;
+				}
+				return verb;
+			});
+			return isAlreadyThere ? state : state.concat(action.verb);
 			break;
 		case 'REMOVE_VERB':
 			return state.filter(verb => verb.id !== action.id);
