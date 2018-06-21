2a2ba9cda647d20536869bb5e9ea7d1f9e78e787
jean-roger orsini <jeanroger.orsini@gmail.com>
2018-06-21 17:15:39 +0900

    m

--- src/components/pages/WordPracticePage.js
+++ src/components/pages/WordPracticePage.js
@@ -2,15 +2,28 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
-const test = true;
-
-const keyDownEvenHandler = e => {
-	e.keyCode === 13 && test && console.log('yo yo');
-};
+const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
 
 const WordPractice = props => {
+	console.log(props);
+
+	const generateQuestion = () => {
+		const wordIndex = getRandomInt(props.verbs.length);
+		const exampleIndex = getRandomInt(
+			props.verbs[wordIndex].exampleList.length
+		);
+		return props.verbs[wordIndex].exampleList[exampleIndex];
+	};
+
+	const keyDownEvenHandler = e => {
+		window.location.pathname === '/word-practice' &&
+			e.keyCode === 13 &&
+			generateAnswer();
+	};
+
+	document.addEventListener('keydown', keyDownEvenHandler);
 	return (
-		<div className="container" keydown={keyDownEvenHandler(event)}>
+		<div className="container">
 			<div>Question</div>
 			<div>Answer</div>
 		</div>
