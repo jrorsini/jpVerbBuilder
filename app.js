8c921b5b0bac99518990a48e2f4447f72658cf6c
Jean-Roger <jeanroger.orsini@gmail.com>
2018-07-09 05:59:44 +0900

    Put all promise's logic into functions.

--- app.js
+++ app.js
@@ -6,52 +6,9 @@ const fs = require('fs');
 const request = require('request');
 const cheerio = require('cheerio');
 
-app.use(cors());
-
-app.get('/search/:word', (req, res) => {
-	request(
-		`https://ejje.weblio.jp/content/${querystring.escape(req.params.word)}`,
-		(error, body, html) => {
-			const $ = cheerio.load(html);
-			const kanji = $('#h1Query').text();
-			const hiragana = $('.summaryL .ruby').text();
-			const meaning = $('.content-explanation').text();
-			const exampleList = [];
-
-			$('.qotC').each((i, e) => {
-				const jp = $(e)
-					.children()
-					.eq(0)
-					.text()
-					.replace('例文帳に追加', '');
-				const en = $(e)
-					.children()
-					.eq(1)
-					.text()
-					.split(/\s\-\s/)[0];
-				jp &&
-					en &&
-					exampleList.push({
-						jp,
-						en
-					});
-			});
-
-			res.send({
-				kanji,
-				hiragana,
-				meaning,
-				exampleList
-			});
-		}
-	);
-});
-
 const execEngSearch = word =>
 	new Promise((resolve, reject) => {
-		// app.get(`/eng-search/${word}`, (req, res) => {
 		request(`https://ejje.weblio.jp/content/${word}`, (error, body, html) => {
-			// querystring.escape(req.params.word)
 			const $ = cheerio.load(html);
 			const word = $('#h1Query').text();
 			const japaneseDefinitions = $('.content-explanation')
@@ -80,9 +37,55 @@ const execEngSearch = word =>
 
 			resolve({ word, japaneseDefinitions, exampleList });
 		});
-		// });
 	});
 
-execEngSearch('blast off').then(res => console.log(res));
+const execJapSearch = word =>
+	new Promise((resolve, reject) => {
+		request(`https://ejje.weblio.jp/content/${word}`, (error, body, html) => {
+			const $ = cheerio.load(html);
+			const kanji = $('#h1Query').text();
+			const hiragana = $('.summaryL .ruby').text();
+			const meaning = $('.content-explanation').text();
+			const exampleList = [];
+
+			$('.qotC').each((i, e) => {
+				const jp = $(e)
+					.children()
+					.eq(0)
+					.text()
+					.replace('例文帳に追加', '');
+				const en = $(e)
+					.children()
+					.eq(1)
+					.text()
+					.split(/\s\-\s/)[0];
+				jp &&
+					en &&
+					exampleList.push({
+						jp,
+						en
+					});
+			});
+
+			resolve({
+				kanji,
+				hiragana,
+				meaning,
+				exampleList
+			});
+		});
+	});
+
+app.use(cors());
+
+app.get('/search/:word', (req, res) => {
+	execJapSearch(querystring.escape(req.params.word)).then(res => res.send(res));
+});
+
+app.get(`/eng-search/:word}`, (req, res) => {
+	execEngSearch(querystring.escape(req.params.word)).then(res => res.send(res));
+});
+
+execEngSearch('pop up').then(res => console.log(res));
 
 app.listen(1234, () => console.log('Up & Running...'));
