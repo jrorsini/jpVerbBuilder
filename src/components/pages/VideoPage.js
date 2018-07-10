e7fc72db2c9cbbd501b50655dee6eba0ec2cadfc
Jean-Roger <jeanroger.orsini@gmail.com>
2018-07-11 00:37:46 +0900

    Added links for each episodes.

--- src/components/pages/VideoPage.js
+++ src/components/pages/VideoPage.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
-const Video = props => <div className="container">video</div>;
+const odArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+const Video = props => (
+	<div className="container">
+		<ul>
+			{odArr.map(e => (
+				<li>
+					<NavLink to={`/videos/episode-${e}`}>Episode{e}</NavLink>
+				</li>
+			))}
+		</ul>
+	</div>
+);
 
 const mapStateToProps = state => {
 	return state;
