efc6f32527bf50ad4ba5ded546100eded6dbd362
Jean-Roger <jeanroger.orsini@gmail.com>
2018-07-10 18:45:33 +0900

    Minor.

--- src/components/pages/ListPage.js
+++ src/components/pages/ListPage.js
@@ -2,9 +2,7 @@ import React from 'react';
 import WordList from '../WordList';
 import { connect } from 'react-redux';
 
-const List = props => (
-	<div className="container">{props.verbs && <WordList />}</div>
-);
+const List = props => <div className="container">{props.words && 'List'}</div>;
 const mapStateToProps = state => {
 	return state;
 };
