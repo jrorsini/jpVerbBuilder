6bf9de57d75f02f15ab51666338045b772be2a72
Jean-Roger <jeanroger.orsini@gmail.com>
2018-07-14 16:36:05 +0900

    Showing words in list.

--- src/components/pages/ListPage.js
+++ src/components/pages/ListPage.js
@@ -2,7 +2,11 @@ import React from 'react';
 import WordList from '../WordList';
 import { connect } from 'react-redux';
 
-const List = props => <div className="container">{props.words && 'List'}</div>;
+const List = props => (
+	<div className="container">
+		{props.words && <ul>{props.words.map(w => <li>{w.word}</li>)}</ul>}
+	</div>
+);
 const mapStateToProps = state => {
 	return state;
 };
