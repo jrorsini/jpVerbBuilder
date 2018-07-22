46319aac824b4ae7a7aa258c6b1d9fced300e093
Jean-Roger <jeanroger.orsini@gmail.com>
2018-07-22 13:31:51 +0900

    Working on the design of the UI.

--- src/components/pages/ListPage.js
+++ src/components/pages/ListPage.js
@@ -3,14 +3,27 @@ import WordList from '../WordList';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
+import { removeWord } from '../../actions/verbs';
+
 const List = props => (
 	<div className="container">
 		{props.words.length > 0 ? (
-			<ul>{props.words.map((w, wId) => <li key={wId}>{w.word}</li>)}</ul>
+			<ul>
+				{props.words.map((w, wId) => (
+					<li key={wId}>
+						{w.word}{' '}
+						<button onClick={() => props.dispatch(removeWord(w.word))}>
+							remove
+						</button>
+					</li>
+				))}
+			</ul>
 		) : (
 			<p className="wordBook__info">
-				No words yet! to look for a word go to the{' '}
-				<NavLink to="/">search</NavLink>
+				No words in your bookmark. To look for a word go to the{' '}
+				<NavLink to="/" className="wordBook__info__link">
+					search
+				</NavLink>
 			</p>
 		)}
 	</div>
