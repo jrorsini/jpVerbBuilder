9201e6e6555c9e3877882f8bd207b03a1f0c06dd
Jean-Roger <jeanroger.orsini@gmail.com>
2018-07-25 13:19:12 +0900

    Getting the timeout thing working.

--- src/components/pages/Pronunciation.js
+++ src/components/pages/Pronunciation.js
@@ -3,11 +3,20 @@ import { connect } from 'react-redux';
 
 import { removeWord } from '../../actions/verbs';
 
-const Pronunciation = props => (
-	<div className="container">
-		<p>Pronunciation</p>
-	</div>
-);
+let count = 0;
+const Pronunciation = props => {
+	while (count < 11) {
+		setTimeout(() => {
+			count++;
+			console.log(count);
+		}, 1000);
+	}
+	return (
+		<div className="container">
+			<p>Pronunciation</p>
+		</div>
+	);
+};
 const mapStateToProps = state => {
 	return state;
 };
