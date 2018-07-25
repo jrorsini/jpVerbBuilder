b5baffa32f6593a277967a03cf83128afdfb086a
Jean-Roger <jeanroger.orsini@gmail.com>
2018-07-25 15:13:07 +0900

    Minor.

--- src/components/pages/Pronunciation.js
+++ src/components/pages/Pronunciation.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
 import { removeWord } from '../../actions/verbs';
 
+const arr = ['あいうえおいうえおあうえおあ'];
+// かきくけこ;
+// さしすせそ;
+// たちつてと;
+// なにぬねの;
+
 const Pronunciation = props => {
-	const stared = (() => {
+	let started = false;
+	const start = () => {
 		let count = 0;
+		started = true;
 		const int = setInterval(() => {
 			console.log(count);
 			count++;
 			if (count > 11) {
 				console.log('object');
+				started = false;
 				clearInterval(int);
 			}
 		}, 500);
-	})();
+	};
 
 	return (
 		<div className="container">
-			<p>Pronunciation</p>
+			<p>
+				Your Pronunciation practice is about to begin, get yourself ready for a
+				couple minute then click on the start button
+			</p>
+			<button
+				onClick={() => {
+					started = true;
+					start();
+				}}
+			>
+				start
+			</button>
 		</div>
 	);
 };
