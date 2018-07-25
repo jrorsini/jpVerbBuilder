df45ba77b14f9a12eb0f159cf2adc1b321ed73c4
Jean-Roger <jeanroger.orsini@gmail.com>
2018-07-25 12:30:38 +0900

    Minor.

--- src/components/WordItem/WordExamplesList.js
+++ src/components/WordItem/WordExamplesList.js
@@ -17,6 +17,22 @@ import {
 
 import { tokenize } from 'kuromojin';
 
+const speechPart = {
+	'*': 'general',
+	名詞: 'noun',
+	動詞: 'verb',
+	副詞: 'adverb',
+	記号: 'symbol',
+	助詞: 'particle',
+	接頭詞: 'prefix',
+	形容詞: 'adjective',
+	接続詞: 'conjuction',
+	感動詞: 'interjection',
+	助動詞: 'auxiliaryVerb',
+	連体詞: 'adnominalAdjective',
+	フィラー: 'filler'
+};
+
 const ExampleList = props => {
 	const word = props.word;
 	const englishTokenizedExample = ex =>
@@ -36,13 +52,27 @@ const ExampleList = props => {
 
 	const japaneseTokenizedExample = ex =>
 		ex.map((e, i) => {
+			if (
+				speechPart[e.pos] === 'verb' ||
+				speechPart[e.pos] === 'noun' ||
+				speechPart[e.pos] === 'adjective'
+			) {
+				console.log(e);
+			}
+
 			return (
 				<span
-					className={` ${
+					className={`${
+						speechPart[e.pos] === 'verb' ||
+						speechPart[e.pos] === 'noun' ||
+						speechPart[e.pos] === 'adjective'
+							? 'exampleList__example__jap'
+							: ''
+					}${
 						e.surface_form.toLowerCase() === word.word.toLowerCase()
 							? 'exampleList__example__jap--highlighted'
 							: ''
-					} ${
+					}${
 						isKatakana(e.surface_form)
 							? 'exampleList__example__jap--katakana'
 							: ''
