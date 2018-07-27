2d01c7afc43e18f66e8d1d0b4b6b7e8ab20f6b18
Jean-Roger <jeanroger.orsini@gmail.com>
2018-07-27 14:57:12 +0900

    Minor.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,5 @@
-// Initialize Firebase
+import * as firebase from 'firebase';
+
 const config = {
 	apiKey: 'AIzaSyAqByuC07TEPz9F5rDTg2uD2QAgDLa_Flc',
 	authDomain: 'jrpan-28272.firebaseapp.com',
@@ -7,4 +8,10 @@ const config = {
 	storageBucket: 'jrpan-28272.appspot.com',
 	messagingSenderId: '224772553556'
 };
+
 firebase.initializeApp(config);
+
+firebase
+	.database()
+	.ref()
+	.set({ name: 'Jean-Roger' });
