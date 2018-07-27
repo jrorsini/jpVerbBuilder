e9efcc34d57f9935f821f5259ed6cf03827079a6
Jean-Roger <jeanroger.orsini@gmail.com>
2018-07-27 13:02:42 +0900

    Setting up firebase.

---
+++ src/firebase/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+const config = {
+	apiKey: 'AIzaSyAqByuC07TEPz9F5rDTg2uD2QAgDLa_Flc',
+	authDomain: 'jrpan-28272.firebaseapp.com',
+	databaseURL: 'https://jrpan-28272.firebaseio.com',
+	projectId: 'jrpan-28272',
+	storageBucket: 'jrpan-28272.appspot.com',
+	messagingSenderId: '224772553556'
+};
+firebase.initializeApp(config);
