59b86e6dbd3dd8a792b8624fe28ed4efc44fc6cd
Jean-Roger <jeanroger.orsini@gmail.com>
2018-07-28 16:41:46 +0900

    Working on the addWord's action.

--- src/actions/words.js
+++ src/actions/words.js
@@ -1,3 +1,5 @@
+import database from '../firebase/firebase';
+
 export const addWord = word => ({
 	type: 'ADD_WORD',
 	word
@@ -7,3 +9,19 @@ export const removeWord = word => ({
 	type: 'REMOVE_WORD',
 	word
 });
+
+export const startAddWord = word => {
+	return dispatch => {
+		database
+			.ref('words')
+			.push(word)
+			.then(ref => {
+				dispatch(
+					addWord({
+						id: ref.key,
+						...word
+					})
+				);
+			});
+	};
+};
