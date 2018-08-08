21e75a51df75f060be4fc0dbe2d6bf8e5428ee6e
Jean-Roger <jeanroger.orsini@gmail.com>
2018-08-08 16:40:03 +0900

    Added endings for 'a', 'e', 'i' and 'te'

--- src/classes/conjugator.js
+++ src/classes/conjugator.js
@@ -1,25 +1,63 @@
 class Conjugator {
-	verbType(type) {
-		return type.match('五段') !== null ? 'godan' : 'ichidan';
-	}
+	teEnding = {
+		る: 'って',
+		つ: 'って',
+		う: 'って',
+		む: 'んで',
+		ぬ: 'んで',
+		ぶ: 'んで',
+		く: 'いて',
+		ぐ: 'いで',
+		す: 'して'
+	};
+
+	aEnding = {
+		る: 'ら',
+		つ: 'た',
+		う: 'わ',
+		む: 'ま',
+		ぬ: 'な',
+		ぶ: 'ば',
+		く: 'か',
+		ぐ: 'が',
+		す: 'さ'
+	};
+
+	iEnding = {
+		る: 'り',
+		つ: 'ち',
+		う: 'い',
+		む: 'み',
+		ぬ: 'に',
+		ぶ: 'び',
+		く: 'き',
+		ぐ: 'ぎ',
+		す: 'し'
+	};
+
+	eEnding = {
+		る: 'れ',
+		つ: 'て',
+		う: 'え',
+		む: 'め',
+		ぬ: 'ね',
+		ぶ: 'べ',
+		く: 'け',
+		ぐ: 'げ',
+		す: 'せ'
+	};
+
+	verbType = type => (type.match('五段') !== null ? 'godan' : 'ichidan');
 
 	teForm(v, t) {
-		const teEnding = {
-			る: 'って',
-			つ: 'って',
-			う: 'って',
-			く: 'いて',
-			ぐ: 'いで',
-			む: 'んで',
-			ぬ: 'んで',
-			ぶ: 'んで',
-			す: 'して'
-		};
+		const l = v.length;
 
 		return this.verbType(t) === 'godan'
-			? v.slice(0, v.length - 1) + teEnding[v.split('').pop()]
-			: v.slice(0, v.length - 1) + 'て';
+			? v.slice(0, l - 1) + teEnding[v.slice(l - 1, l)]
+			: v.slice(0, l - 1) + 'て';
 	}
+
+	aForm(v, t) {}
 }
 
 export const c = new Conjugator();
